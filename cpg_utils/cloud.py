1ceb96bb00452ca340cd405919a6b8ceed3d9d72
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-07-21 09:44:02 +1000

    Bump version: 2.0.0 → 2.1.0: Add convenience functions for reading / writing GCP secrets (#5)
    * Add convenience functions for reading / writing GCP secrets.
    
    * Bump version: 2.0.0 → 2.1.0

--- cpg_utils/cloud.py
+++ cpg_utils/cloud.py
@@ -1,6 +1,9 @@
 """Convenience functions related to cloud infrastructure."""
 
+from typing import Optional
 from google.auth import jwt
+from google.cloud import secretmanager
+import google.api_core.exceptions
 
 
 def email_from_id_token(id_token: str) -> str:
@@ -11,3 +14,41 @@ def email_from_id_token(id_token: str) -> str:
     This function assumes that the token has been verified beforehand."""
 
     return jwt.decode(id_token, verify=False)['email']
+
+
+def read_secret(project_id: str, secret_name: str) -> Optional[str]:
+    """Reads the latest version of a GCP Secret Manager secret.
+
+    Returns None if the secret doesn't exist."""
+
+    secret_manager = secretmanager.SecretManagerServiceClient()
+    secret_path = secret_manager.secret_path(project_id, secret_name)
+
+    try:
+        response = secret_manager.access_secret_version(
+            request={'name': f'{secret_path}/versions/latest'}
+        )
+    except google.api_core.exceptions.ClientError:
+        # Fail gracefully if there's no secret version yet.
+        return None
+
+    return response.payload.data.decode('UTF-8')
+
+
+def write_secret(project_id: str, secret_name: str, secret_value: str) -> None:
+    """Adds a new version for a GCP Secret Manager secret and disables all previous versions."""
+
+    secret_manager = secretmanager.SecretManagerServiceClient()
+    secret_path = secret_manager.secret_path(project_id, secret_name)
+
+    response = secret_manager.add_secret_version(
+        request={
+            'parent': secret_path,
+            'payload': {'data': secret_value.encode('UTF-8')},
+        }
+    )
+
+    # Disable all previous versions.
+    for version in secret_manager.list_secret_versions(request={'parent': secret_path}):
+        if version.name != response.name:  # Don't disable the latest version.
+            secret_manager.disable_secret_version(request={'name': version.name})
