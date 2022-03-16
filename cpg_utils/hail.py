eb860d170a4c20d067d98782fa687b9e7b49891a
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-03-17 09:58:26 +1100

    Bump version: 2.2.0 → 2.3.0: Add hail.remote_tmpdir method (#14)
    * Add hail.remote_tmpdir method
    
    That will be useful for replacing the deprecated `bucket` parameter used in the analysis-runner.
    
    * Bump version: 2.2.0 → 2.3.0

--- cpg_utils/hail.py
+++ cpg_utils/hail.py
@@ -13,18 +13,16 @@ def init_query_service():
 
     billing_project = os.getenv('HAIL_BILLING_PROJECT')
     assert billing_project
-    hail_bucket = os.getenv('HAIL_BUCKET')
-    assert hail_bucket
     return asyncio.get_event_loop().run_until_complete(
         hl.init_service(
             default_reference='GRCh38',
             billing_project=billing_project,
-            remote_tmpdir=f'gs://{hail_bucket}/batch-tmp',
+            remote_tmpdir=remote_tmpdir(),
         )
     )
 
 
-def copy_common_env(job: hb.job.Job):
+def copy_common_env(job: hb.job.Job) -> None:
     """Copies common environment variables that we use to run Hail jobs.
 
     These variables are typically set up in the analysis-runner driver, but need to be
@@ -47,3 +45,13 @@ def copy_common_env(job: hb.job.Job):
         val = os.getenv(key)
         if val:
             job.env(key, val)
+
+
+def remote_tmpdir() -> str:
+    """Returns the remote_tmpdir to use for Hail initialization.
+
+    Requires the HAIL_BUCKET environment variable to be set."""
+
+    hail_bucket = os.getenv('HAIL_BUCKET')
+    assert hail_bucket
+    return f'gs://{hail_bucket}/batch-tmp'
