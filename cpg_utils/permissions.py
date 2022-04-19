e93bf95aa59f3f2663f42cf6d9698242f9e471b0
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-04-19 17:46:51 +1000

    Add get_group_members for supporting GCS access-group cache

---
+++ cpg_utils/permissions.py
@@ -0,0 +1,40 @@
+"""Convenience functions related to permissions """
+from typing import List
+import os
+import re
+
+from cloudpathlib import AnyPath
+
+group_name_santise_matcher = re.compile(r'[^\w_-]+')
+
+DEFAULT_GROUP_CACHE_LOCATION = os.getenv(
+    'CPG_GROUP_CACHE_LOCATION', 'gs://cpg-group-cache'
+)
+
+
+def group_name_to_filename(
+    group_name: str,
+    version: str,
+    group_cache_location: str = DEFAULT_GROUP_CACHE_LOCATION,
+) -> str:
+    """
+    Convert google group name to GCS safe directory name.
+    Replace all non [alphanumeric characters and _-] with '_'.
+
+    >>> _group_name_to_filename('my-group_with+chars@populationgenomics.org.au')
+    'my-group_with_chars_populationgenomics_org_au'
+    """
+    base = group_name_santise_matcher.sub('_', group_name)
+    return os.path.join(group_cache_location, base, version)
+
+
+def get_group_members(
+    group: str, group_cache_location=DEFAULT_GROUP_CACHE_LOCATION
+) -> List[str]:
+    """Returns the members of the given group.
+
+    :param group: str The group to query.
+    :returns: List[str] Members of the group
+    """
+    filename = group_name_to_filename(group, 'latest', group_cache_location)
+    return AnyPath(filename).read_text().split(',')
