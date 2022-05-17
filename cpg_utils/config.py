e94b7e5d2ebef2c2f8abe258b5f62da4639276e4
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-05-17 21:45:51 +1000

    Use a config file instead of environment variables

---
+++ cpg_utils/config.py
@@ -0,0 +1,62 @@
+"""Provides access to config variables."""
+
+import os
+from typing import Any, Dict, Optional, MutableMapping
+from cloudpathlib import AnyPath
+import toml
+
+_config_cache: Dict[str, MutableMapping[str, Any]] = dict()
+
+
+def get_config(config_path: Optional[str] = None) -> MutableMapping[str, Any]:
+    """Returns a configuration dictionary.
+
+    Examples
+    --------
+    Here's a typical configuration file in TOML format:
+
+    [hail]
+    billing_project = "tob-wgs"
+    bucket = "cpg-tob-wgs-hail"
+
+    [workflow]
+    access_level = "test"
+    dataset = "tob-wgs"
+    dataset_gcp_project = "tob-wgs"
+    driver_image = "australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:36c6d4548ef347f14fd34a5b58908057effcde82-hail-ad1fc0e2a30f67855aee84ae9adabc3f3135bd47"
+    image_registry_prefix = "australia-southeast1-docker.pkg.dev/cpg-common/images"
+    reference_prefix = "gs://cpg-reference"
+    output_prefix = "plasma/chr22/v6"
+
+    Notes
+    -----
+    Caches the result based on the config path alone.
+
+    Parameters
+    ----------
+    config_path: str, optional
+        A cloudpathlib-compatible path to a TOML file containing the configuration. If
+        this parameter not provided, the CPG_CONFIG_PATH environment variable is used
+        instead.
+
+    Returns
+    -------
+    dict
+    """
+
+    if config_path is None:
+        config_path = os.getenv('CPG_CONFIG_PATH')
+    assert config_path
+
+    cached = _config_cache.get(config_path)
+    if cached is not None:
+        return cached
+
+    with AnyPath(config_path).open() as f:
+        config_str = f.read()
+
+    # Print the config content, which is helpful for debugging.
+    print(f'Configuration at {config_path}:\n{config_str}')
+    config = _config_cache[config_path] = toml.loads(config_str)
+
+    return config
--- cpg_utils/hail_batch.py
+++ cpg_utils/hail_batch.py
@@ -12,6 +12,8 @@ import hailtop.batch as hb
 from cloudpathlib import CloudPath
 from cloudpathlib.anypath import to_anypath
 
+from config import get_config
+
 
 GCLOUD_AUTH_COMMAND = (
     'gcloud -q auth activate-service-account --key-file=/gsa-key/key.json'
@@ -21,20 +23,17 @@ GCLOUD_AUTH_COMMAND = (
 def init_batch(**kwargs):
     """
     Initializes the Hail Query Service from within Hail Batch.
-    Requires the HAIL_BILLING_PROJECT and HAIL_BUCKET environment variables to be set.
+    Requires the `hail/billing_project` and `hail/bucket` config variables to be set.
 
     Parameters
     ----------
     kwargs : keyword arguments
         Forwarded directly to `hl.init_batch`.
     """
-
-    billing_project = os.getenv('HAIL_BILLING_PROJECT')
-    assert billing_project
     return asyncio.get_event_loop().run_until_complete(
         hl.init_batch(
             default_reference='GRCh38',
-            billing_project=billing_project,
+            billing_project=get_config()['hail']['billing_project'],
             remote_tmpdir=remote_tmpdir(),
             **kwargs,
         )
@@ -48,20 +47,11 @@ def copy_common_env(job: hb.batch.job.Job) -> None:
     passed through for "batch-in-batch" use cases.
 
     The environment variable values are extracted from the current process and
-    copied to the environment dictionary of the given Hail Batch job."""
-
-    for key in (
-        'CPG_ACCESS_LEVEL',
-        'CPG_DATASET',
-        'CPG_DATASET_GCP_PROJECT',
-        'CPG_DATASET_PATH',
-        'CPG_DRIVER_IMAGE',
-        'CPG_IMAGE_REGISTRY_PREFIX',
-        'CPG_REFERENCE_PREFIX',
-        'CPG_OUTPUT_PREFIX',
-        'HAIL_BILLING_PROJECT',
-        'HAIL_BUCKET',
-    ):
+    copied to the environment dictionary of the given Hail Batch job.
+    """
+    # If possible, please don't add new environment variables here, but instead add
+    # config variables.
+    for key in ('CPG_CONFIG_PATH',):
         val = os.getenv(key)
         if val:
             job.env(key, val)
@@ -70,12 +60,9 @@ def copy_common_env(job: hb.batch.job.Job) -> None:
 def remote_tmpdir(hail_bucket: Optional[str] = None) -> str:
     """Returns the remote_tmpdir to use for Hail initialization.
 
-    If `hail_bucket` is not specified explicitly, requires the HAIL_BUCKET environment variable to be set."""
-
-    if not hail_bucket:
-        hail_bucket = os.getenv('HAIL_BUCKET')
-        assert hail_bucket
-    return f'gs://{hail_bucket}/batch-tmp'
+    If `hail_bucket` is not specified explicitly, requires the `hail/bucket` config variable to be set.
+    """
+    return f'gs://{hail_bucket or get_config()["hail"]["bucket"]}/batch-tmp'
 
 
 def dataset_path(suffix: str, category: Optional[str] = None) -> str:
@@ -83,7 +70,7 @@ def dataset_path(suffix: str, category: Optional[str] = None) -> str:
     Returns a full path for the current dataset, given a category and path suffix.
 
     This is useful for specifying input files, as in contrast to the output_path
-    function, dataset_path does _not_ take the CPG_OUTPUT_PREFIX environment variable
+    function, dataset_path does _not_ take the `workflow/output_prefix` config variable
     into account.
 
     Examples
@@ -100,8 +87,8 @@ def dataset_path(suffix: str, category: Optional[str] = None) -> str:
     Notes
     -----
     Requires either the
-    * `CPG_DATASET` and `CPG_ACCESS_LEVEL` environment variables, or the
-    * `CPG_DATASET_PATH` environment variable
+    * `workflow/dataset` and `workflow/access_level` config variables, or the
+    * `workflow/dataset_path` config variable
     to be set, where the former takes precedence.
 
     Parameters
@@ -118,8 +105,8 @@ def dataset_path(suffix: str, category: Optional[str] = None) -> str:
     -------
     str
     """
-    dataset = os.getenv('CPG_DATASET')
-    access_level = os.getenv('CPG_ACCESS_LEVEL')
+    dataset = get_config()['workflow'].get('dataset')
+    access_level = get_config()['workflow'].get('access_level')
 
     if dataset and access_level:
         namespace = 'test' if access_level == 'test' else 'main'
@@ -129,8 +116,7 @@ def dataset_path(suffix: str, category: Optional[str] = None) -> str:
             category = f'{namespace}-{category}'
         prefix = f'cpg-{dataset}-{category}'
     else:
-        prefix = os.getenv('CPG_DATASET_PATH') or ''  # coerce to str
-        assert prefix
+        prefix = get_config()['workflow']['dataset_path']
 
     return os.path.join('gs://', prefix, suffix)
 
@@ -138,12 +124,12 @@ def dataset_path(suffix: str, category: Optional[str] = None) -> str:
 def output_path(suffix: str, category: Optional[str] = None) -> str:
     """Returns a full path for the given category and path suffix.
 
-    In contrast to the dataset_path function, output_path takes the CPG_OUTPUT_PREFIX
-    environment variable into account.
+    In contrast to the dataset_path function, output_path takes the `workflow/output_prefix`
+    config variable into account.
 
     Examples
     --------
-    If using the analysis-runner, the CPG_OUTPUT_PREFIX would be set to the argument
+    If using the analysis-runner, the `workflow/output_prefix` would be set to the argument
     provided using the --output argument, e.g.
     `--dataset fewgenomes --access-level test --output 1kg_pca/v42`:
     will use '1kg_pca/v42' as the base path to build upon in this method
@@ -156,7 +142,7 @@ def output_path(suffix: str, category: Optional[str] = None) -> str:
 
     Notes
     -----
-    Requires the `CPG_OUTPUT_PREFIX` environment variable to be set, in addition to the
+    Requires the `workflow/output_prefix` config variable to be set, in addition to the
     requirements for `dataset_path`.
 
     Parameters
@@ -173,9 +159,9 @@ def output_path(suffix: str, category: Optional[str] = None) -> str:
     -------
     str
     """
-    output = os.getenv('CPG_OUTPUT_PREFIX')
-    assert output
-    return dataset_path(os.path.join(output, suffix), category)
+    return dataset_path(
+        os.path.join(get_config()['workflow']['output_prefix'], suffix), category
+    )
 
 
 def image_path(suffix: str) -> str:
@@ -188,7 +174,7 @@ def image_path(suffix: str) -> str:
 
     Notes
     -----
-    Requires the `CPG_IMAGE_REGISTRY_PREFIX` environment variable to be set.
+    Requires the `workflow/image_registry_prefix` config variable to be set.
 
     Parameters
     ----------
@@ -199,9 +185,7 @@ def image_path(suffix: str) -> str:
     -------
     str
     """
-    prefix = os.getenv('CPG_IMAGE_REGISTRY_PREFIX')
-    assert prefix
-    return f'{prefix}/{suffix}'
+    return f'{get_config()["workflow"]["image_registry_prefix"]}/{suffix}'
 
 
 def reference_path(suffix: str) -> Union[CloudPath, Path]:
@@ -214,21 +198,19 @@ def reference_path(suffix: str) -> Union[CloudPath, Path]:
 
     Notes
     -----
-    Requires the `CPG_REFERENCE_PREFIX` environment variable to be set.
+    Requires the `workflow/reference_prefix` config variable to be set.
 
     Parameters
     ----------
     suffix : str
-        Describes path relative to CPG_REFERENCE_PREFIX.
+        Describes path relative to the reference prefix.
 
     Returns
     -------
     str
     """
-    prefix = os.getenv('CPG_REFERENCE_PREFIX')
-    assert prefix
-    suffix = suffix.strip('/')  # leading slash results in `prefix` entirely ignored
-    return to_anypath(prefix) / suffix
+    # A leading slash results in the prefix being ignored, therefore use strip below.
+    return to_anypath(get_config()['workflow']['reference_prefix']) / suffix.strip('/')
 
 
 def authenticate_cloud_credentials_in_job(
