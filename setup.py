766d145cd6150ba0f412859bd4dd542f4772c80b
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-05-31 09:23:39 +1000

    Bump version: 4.2.2 → 4.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 setuptools.setup(
     name='cpg-utils',
     # This tag is automatically updated by bumpversion
-    version='4.2.2',
+    version='4.2.3',
     description='Library of convenience functions specific to the CPG',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
