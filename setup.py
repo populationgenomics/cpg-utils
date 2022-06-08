ecbf6d52681118d3279a993efec04f704b8fa6ed
Vlad Savelyev <vladislav.savelyev@populationgenomics.org.au>
2022-06-09 09:54:45 +1000

    Bump version: 4.3.3 → 4.3.4: Use driver image instead of deprecated sm-api image (#51)
    * Use driver image instead of deprecated sm-api image
    
    * Bump version: 4.3.3 → 4.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 setuptools.setup(
     name='cpg-utils',
     # This tag is automatically updated by bumpversion
-    version='4.3.3',
+    version='4.3.4',
     description='Library of convenience functions specific to the CPG',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
