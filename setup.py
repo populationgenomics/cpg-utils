ee7481ecc6b58582b2098bdf1905fc7a45ea536e
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-08-29 13:11:24 +1000

    Bump version: 4.5.2 → 4.5.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name='cpg-utils',
     # This tag is automatically updated by bumpversion
-    version='4.5.2',
+    version='4.5.3',
     description='Library of convenience functions specific to the CPG',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
