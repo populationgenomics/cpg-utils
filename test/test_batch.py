a1de8abf7e7974f752df4626b5fdc05aa9452a99
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-09-29 16:09:50 +1000

    Fix lint

--- test/test_batch.py
+++ test/test_batch.py
@@ -9,6 +9,7 @@ from cpg_utils import to_path, Path
 from cpg_utils.config import set_config_paths, update_dict
 from cpg_utils.hail_batch import dataset_path, command
 from cpg_utils.workflows.utils import timestamp
+from cpg_utils.workflows.batch import get_batch
 
 tmp_dir_path = to_path(__file__).parent / 'results' / timestamp()
 tmp_dir_path = tmp_dir_path.absolute()
@@ -78,8 +79,6 @@ def test_batch_python_job():
     Testing calling a python job.
     """
     _set_config(tmp_dir_path)
-
-    from cpg_utils.workflows.batch import get_batch
 
     b = get_batch('Test batch python job')
     j = b.new_python_job('Test python job')
