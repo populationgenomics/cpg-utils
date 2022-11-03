6afb50b0c3d60b8025125f831c91875b6b9643bf
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-11-03 14:45:08 +1100

    Update version pin to be consistent with the hail package

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     license='MIT',
     packages=find_packages(),
     install_requires=[
-        'google-auth',
+        'google-auth>=1.27.0,<2',
         'google-cloud-secret-manager',
         'cloudpathlib[all]',
         'toml',
