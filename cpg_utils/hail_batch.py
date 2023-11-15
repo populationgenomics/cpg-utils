5a5f9705988442621078bf58720205c0b290dc43
Matt Welland <mattwellie@gmail.com>
2023-11-15 20:01:25 +1100

    Migrate get_batch wrapper to cpg-utils (#145)
    * Migrate get_batch wrapper from cpg_workflows
    
    * Add ar-guid + remove config copying
    
    * Bump version: 4.17.1 → 4.18.0
    ---------
    Co-authored-by: Michael Franklin <illusional@users.noreply.github.com>

--- cpg_utils/hail_batch.py
+++ cpg_utils/hail_batch.py
@@ -2,10 +2,11 @@
 
 import asyncio
 import inspect
+import logging
 import os
+import tempfile
 import textwrap
-import typing
-from typing import List, Optional, Union
+from typing import Dict, List, Literal, Optional, Union
 
 import hail as hl
 import hailtop.batch as hb
@@ -26,6 +27,230 @@ export GOOGLE_APPLICATION_CREDENTIALS=/gsa-key/key.json
 gcloud -q auth activate-service-account \
 --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 """
+
+
+_batch: Optional['Batch'] = None
+
+
+def get_batch(
+    name: str | None = None,
+    *,
+    default_python_image: str | None = None,
+    attributes: Optional[Dict[str, str]] = None,
+    **kwargs,
+) -> 'Batch':
+    """
+    Wrapper around Hail's `Batch` class, which allows to register created jobs
+    This has been migrated (currently duplicated) out of cpg_workflows
+
+    Parameters
+    ----------
+    name : str, optional, name for the batch
+    default_python_image : str, optional, default python image to use
+
+    Returns
+    -------
+    If there are scheduled jobs, return the batch
+    If there are no jobs to create, return None
+    """
+    global _batch  # pylint: disable=global-statement
+    backend: hb.Backend
+    if _batch is None:
+        if get_config()['hail'].get('backend', 'batch') == 'local':
+            logging.info('Initialising Hail Batch with local backend')
+            backend = hb.LocalBackend(
+                tmp_dir=tempfile.mkdtemp('batch-tmp'),
+            )
+        else:
+            logging.info('Initialising Hail Batch with service backend')
+            backend = hb.ServiceBackend(
+                billing_project=get_config()['hail']['billing_project'],
+                remote_tmpdir=dataset_path('batch-tmp', category='tmp'),
+                token=os.environ.get('HAIL_TOKEN'),
+            )
+        _batch = Batch(
+            name=name or get_config()['workflow'].get('name'),
+            backend=backend,
+            pool_label=get_config()['hail'].get('pool_label'),
+            cancel_after_n_failures=get_config()['hail'].get('cancel_after_n_failures'),
+            default_timeout=get_config()['hail'].get('default_timeout'),
+            default_memory=get_config()['hail'].get('default_memory'),
+            default_python_image=default_python_image
+            or get_config()['workflow']['driver_image'],
+            attributes=attributes,
+            **kwargs,
+        )
+    return _batch
+
+
+class Batch(hb.Batch):
+    """
+    Thin subclass of the Hail `Batch` class. The aim is to be able to register
+    created jobs, in order to print statistics before submitting the Batch.
+    """
+
+    def __init__(
+        self,
+        name,
+        backend,
+        *,
+        pool_label: Optional[str] = None,
+        attributes: Optional[Dict[str, str]] = None,
+        **kwargs,
+    ):
+        _attributes = attributes or {}
+        if AR_GUID_NAME not in _attributes:
+            _attributes[AR_GUID_NAME] = try_get_ar_guid()
+
+        super().__init__(name, backend, attributes=_attributes, **kwargs)
+        # Job stats registry:
+        self.job_by_label: Dict = {}
+        self.job_by_stage: Dict = {}
+        self.job_by_tool: Dict = {}
+        self.total_job_num = 0
+        self.pool_label = pool_label
+
+    def _process_job_attributes(
+        self,
+        name: str | None = None,
+        attributes: dict | None = None,
+    ) -> tuple[str, dict[str, str]]:
+        """
+        Use job attributes to make the job name more descriptive, and add
+        labels for Batch pre-submission stats.
+        """
+        if not name:
+            raise ValueError('Error: job name must be defined')
+
+        self.total_job_num += 1
+
+        attributes = attributes or {}
+        stage = attributes.get('stage')
+        dataset = attributes.get('dataset')
+        sequencing_group = attributes.get('sequencing_group')
+        participant_id = attributes.get('participant_id')
+        sequencing_groups: set[str] = set(attributes.get('sequencing_groups') or [])
+        if sequencing_group:
+            sequencing_groups.add(sequencing_group)
+        part = attributes.get('part')
+        label = attributes.get('label', name)
+        tool = attributes.get('tool')
+        if not tool and name.endswith('Dataproc cluster'):
+            tool = 'hailctl dataproc'
+
+        # pylint: disable=W1116
+        assert isinstance(stage, str | None)
+        assert isinstance(dataset, str | None)
+        assert isinstance(sequencing_group, str | None)
+        assert isinstance(participant_id, str | None)
+        assert isinstance(part, str | None)
+        assert isinstance(label, str | None)
+
+        name = make_job_name(
+            name=name,
+            sequencing_group=sequencing_group,
+            participant_id=participant_id,
+            dataset=dataset,
+            part=part,
+        )
+
+        if label not in self.job_by_label:
+            self.job_by_label[label] = {'job_n': 0, 'sequencing_groups': set()}
+        self.job_by_label[label]['job_n'] += 1
+        self.job_by_label[label]['sequencing_groups'] |= sequencing_groups
+
+        if stage not in self.job_by_stage:
+            self.job_by_stage[stage] = {'job_n': 0, 'sequencing_groups': set()}
+        self.job_by_stage[stage]['job_n'] += 1
+        self.job_by_stage[stage]['sequencing_groups'] |= sequencing_groups
+
+        if tool not in self.job_by_tool:
+            self.job_by_tool[tool] = {'job_n': 0, 'sequencing_groups': set()}
+        self.job_by_tool[tool]['job_n'] += 1
+        self.job_by_tool[tool]['sequencing_groups'] |= sequencing_groups
+
+        attributes['sequencing_groups'] = list(sorted(list(sequencing_groups)))
+        fixed_attrs = {k: str(v) for k, v in attributes.items()}
+        return name, fixed_attrs
+
+    def run(self, **kwargs):  # pylint: disable=R1710,W0221
+        """
+        Execute a batch. Overridden to print pre-submission statistics.
+        Pylint disables:
+        - R1710: Either all return statements in a function should return an expression,
+          or none of them should.
+          - if no jobs are present, no batch is returned. Hail should have this behaviour...
+        - W0221: Arguments number differs from overridden method
+          - this wrapper makes use of **kwargs, which is being passed to the super().run() method
+        """
+        if not self._jobs:
+            logging.error('No jobs to submit')
+            return
+
+        for job in self._jobs:
+            job.name, job.attributes = self._process_job_attributes(
+                job.name, job.attributes
+            )
+            # We only have dedicated pools for preemptible machines.
+            # _preemptible defaults to None, so check explicitly for False.
+            # pylint: disable=W0212
+            if self.pool_label and job._preemptible is not False:
+                job._pool_label = self.pool_label
+            copy_common_env(job)
+
+        logging.info(f'Will submit {self.total_job_num} jobs')
+
+        def _print_stat(prefix: str, _d: dict, default_label: str | None = None):
+            m = (prefix or ' ') + '\n'
+            for label, stat in _d.items():
+                label = label or default_label
+                msg = f'{stat["job_n"]} job'
+                if stat['job_n'] > 1:
+                    msg += 's'
+                if (sg_count := len(stat['sequencing_groups'])) > 0:
+                    msg += f' for {sg_count} sequencing group'
+                    if sg_count > 1:
+                        msg += 's'
+                m += f'  {label}: {msg}'
+            logging.info(m)
+
+        _print_stat(
+            'Split by stage:', self.job_by_stage, default_label='<not in stage>'
+        )
+        _print_stat(
+            'Split by tool:', self.job_by_tool, default_label='<tool is not defined>'
+        )
+
+        kwargs.setdefault('dry_run', get_config()['hail'].get('dry_run'))
+        kwargs.setdefault(
+            'delete_scratch_on_exit', get_config()['hail'].get('delete_scratch_on_exit')
+        )
+        if isinstance(self._backend, hb.LocalBackend):
+            # Local backend does not support "wait"
+            if 'wait' in kwargs:
+                del kwargs['wait']
+        return super().run(**kwargs)
+
+
+def make_job_name(
+    name: str,
+    sequencing_group: str | None = None,
+    participant_id: str | None = None,
+    dataset: str | None = None,
+    part: str | None = None,
+) -> str:
+    """
+    Extend the descriptive job name to reflect job attributes.
+    """
+    if sequencing_group and participant_id:
+        sequencing_group = f'{sequencing_group}/{participant_id}'
+    if sequencing_group and dataset:
+        name = f'{dataset}/{sequencing_group}: {name}'
+    elif dataset:
+        name = f'{dataset}: {name}'
+    if part:
+        name += f', {part}'
+    return name
 
 
 def init_batch(**kwargs):
@@ -528,8 +753,7 @@ python3 script.py
 
 
 def start_query_context(
-    query_backend: typing.Literal['spark', 'batch', 'local', 'spark_local']
-    | None = None,
+    query_backend: Literal['spark', 'batch', 'local', 'spark_local'] | None = None,
     log_path: str | None = None,
     dataset: str | None = None,
     billing_project: str | None = None,
