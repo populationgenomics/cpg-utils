81ca131159398417cfd0a3bf24dd5fb961b059db
Matt Welland <mattwellie@gmail.com>
2024-02-13 16:15:04 +1100

    Allow init_batch overrides in Query_command (#151)
    * Allows for more manual override of init_batch params in query_command
    
    * Bump version: 4.18.3 → 4.19.0

--- cpg_utils/hail_batch.py
+++ cpg_utils/hail_batch.py
@@ -747,6 +747,7 @@ def query_command(
     setup_gcp: bool = False,
     setup_hail: bool = True,
     packages: Optional[List[str]] = None,
+    init_batch_args: dict[str, str | int] | None = None,
 ) -> str:
     """
     Construct a command to run a python function inside a Hail Batch job.
@@ -755,10 +756,23 @@ def query_command(
     Run a Python Hail Query function inside a Hail Batch job.
     Constructs a command string to use with job.command().
     If hail_billing_project is provided, Hail Query will be initialised.
+
+    init_batch_args can be used to pass additional arguments to init_batch.
+    this is a dict of args, which will be placed into the batch initiation command
+    e.g. {'worker_memory': 'highmem'} -> 'init_batch(worker_memory="highmem")'
     """
-    init_hail_code = """
+
+    # translate any input arguments into an embeddable String
+    if init_batch_args:
+        batch_overrides = ', '.join(
+            f'{k}={repr(v)}' for k, v in init_batch_args.items()
+        )
+    else:
+        batch_overrides = ''
+
+    init_hail_code = f"""
 from cpg_utils.hail_batch import init_batch
-init_batch()
+init_batch({batch_overrides})
 """
 
     python_code = f"""
