3a6a31b348f48897f92ffd913e6cf72c396c1391
Michael Franklin <illusional@users.noreply.github.com>
2024-04-14 19:59:30 +1000

    Bump version: 5.0.0 → 5.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 setup(
     name='cpg-utils',
     # This tag is automatically updated by bumpversion
-    version='5.0.0',
+    version='5.0.1',
     description='Library of convenience functions specific to the CPG',
     long_description=long_description,
     long_description_content_type='text/markdown',
