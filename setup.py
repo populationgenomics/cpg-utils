3d96615093674cfffbb2ae10bde146770bfb88a4
Milo Hyben <milo.hyben@gmail.com>
2024-05-17 12:09:49 +1000

    Bump version: 5.0.9 → 5.0.10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 setup(
     name='cpg-utils',
     # This tag is automatically updated by bumpversion
-    version='5.0.9',
+    version='5.0.10',
     description='Library of convenience functions specific to the CPG',
     long_description=long_description,
     long_description_content_type='text/markdown',
