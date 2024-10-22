8ca558591dca98a7af4f2e28e1a09c6d7a7dcb16
Matt Welland <mattwellie@gmail.com>
2024-10-23 10:55:33 +1100

    Quit after aborting (#176)
    * quit after aborting
    
    * Bump version: 5.1.0 → 5.1.1

--- cpg_utils/cromwell.py
+++ cpg_utils/cromwell.py
@@ -483,6 +483,9 @@ def watch_workflow(  # noqa: C901
             and (datetime.now() - start).total_seconds() >= time_limit_seconds
         ):
             # time to die, Mr. Cromwell
+            logger.info(
+                f'This job has exceeded the max permitted runtime ({time_limit_seconds}s), and will be aborted',
+            )
             auth_header = {'Authorization': f'Bearer {_get_cromwell_oauth_token()}'}
             r = requests.post(abort_url, headers=auth_header, timeout=10)
             if not r.ok:
@@ -491,6 +494,9 @@ def watch_workflow(  # noqa: C901
                 )
                 _remaining_exceptions -= 1
                 continue
+            # quit if we successfully aborted
+            logger.info(f'Successfully aborted workflow {workflow_id}')
+            return
 
         if _remaining_exceptions <= 0:
             raise CromwellError('Unreachable')
