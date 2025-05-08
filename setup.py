e58d94d9b90c799081c77e7471fce7f25d795f52
John Marshall <john.marshall@populationgenomics.org.au>
2025-05-08 15:07:57 +1200

    Bump version: 5.2.1 → 5.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 setup(
     name='cpg-utils',
     # This tag is automatically updated by bumpversion
-    version='5.2.1',
+    version='5.3.0',
     description='Library of convenience functions specific to the CPG',
     long_description=long_description,
     long_description_content_type='text/markdown',
