5919879578e3f5f8e636b9f691ff12095ef7f52a
John Marshall <john.marshall@populationgenomics.org.au>
2025-06-19 20:41:53 +1200

    Compress sequencing_groups job attribute when it is very large (#183)
    Add _pack_attribute() to compress attribute values if necessary.
    Suppress annoying ruff warnings about "incomprehensible" numeric constants.
    
    Pop 'sequencing_groups' from the incoming attributes as we may not replace
    the existing item later. Retain `or []` to continue to guard against the
    unlikely event of the raw incoming value being None rather than [].
    
    Bump version: 5.3.1 → 5.4.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 setup(
     name='cpg-utils',
     # This tag is automatically updated by bumpversion
-    version='5.3.1',
+    version='5.4.0',
     description='Library of convenience functions specific to the CPG',
     long_description=long_description,
     long_description_content_type='text/markdown',
