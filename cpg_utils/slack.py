27c54c4fa321b6dee708e8a5cec8e4b4c62cec5d
John Marshall <john.marshall@populationgenomics.org.au>
2025-12-05 10:45:04 +1300

    Add optional title/filename parameters to upload_file()

--- cpg_utils/slack.py
+++ cpg_utils/slack.py
@@ -66,7 +66,12 @@ def send_message(text: str) -> None:
         logging.error(f'Error posting to Slack: {err}')
 
 
-def upload_file(content: bytes, comment: str) -> None:
+def upload_file(
+    content: bytes,
+    comment: str,
+    title: str | None = None,
+    filename: str | None = None,
+) -> None:
     """Uploads `content` to Slack with the given text `comment`, reading credentials from the config."""
     slack_client = _get_slack_sdk().WebClient(token=_get_token())
     try:
@@ -74,6 +79,8 @@ def upload_file(content: bytes, comment: str) -> None:
             channel=_get_channel(),
             content=content,
             initial_comment=comment,
+            title=title,
+            filename=filename,
         )
     except _get_slack_sdk().errors.SlackApiError as err:
         logging.error(f'Error posting to Slack: {err}')
