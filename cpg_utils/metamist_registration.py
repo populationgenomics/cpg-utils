9951f2710a2f64f3e92dc6b4461a310c3538410b
MattWellie <mattwellie@gmail.com>
2026-06-26 12:43:55 +1000

    maybe work as CLI and imported method

--- cpg_utils/metamist_registration.py
+++ cpg_utils/metamist_registration.py
@@ -28,64 +28,32 @@ python3 -m cpg_utils.metamist_registration \
     --meta stage=name sequencing_type=genome \
     --cohorts COH1234 COH5678
 
+# imported equivalent using a method call
+from cpg_utils.metamist_registration import create_new
+create_new(
+    project=<project>,
+    output=<path/to/primary/output>,
+    analysis_type=cram,
+    meta={'stage': 'name', 'sequencing_type': 'genome'},
+    cohorts=['COH1234', 'COH5678'],
+)
+
 This will produce analysis records where:
  - the analysis record is of a primary type defined by --type
- - the analysis record has an 'output' value, defined by --output
+ - the analysis record has an 'outputs' dictionary
+ - outputs.path is the value of --output
+ - if used, secondary files will be nested inside `analysis.outputs.secondary_files.TYPE.path`
+ - if used, analysis.meta will contain all the meta key=value pairs
 """
 
-import argparse
+import argparse  # noqa: I001
 import json
 import sys
 
-from typing import TypeAlias
-
-from metamist import graphql
+from metamist import exceptions, graphql
 
 
-RecursiveDict: TypeAlias = dict[str, 'str | RecursiveDict']
-
-
-def create_output_block(primary: str, type: str, secondary: list[str]) -> RecursiveDict:
-    """Populates the output dict based on the provided arguments."""
-
-    # create the outputs dictionary, with the primary file's full path at the root
-    outputs: RecursiveDict = {
-        'basename': primary,
-    }
-
-    # take the list of key=value strings, snap'em, and add each to the secondary files list
-    if secondary:
-        secondary_kv: RecursiveDict = {}
-        for keyvaluepair in secondary:
-            key, value = keyvaluepair.split('=')
-            secondary_kv[key] = {
-                'basename': value,
-            }
-
-        outputs['secondary_files'] = secondary_kv
-
-    return outputs
-
-
-def main():
-    parser = argparse.ArgumentParser(description='Record MultiQC results to Metamist.')
-    parser.add_argument('--project', required=True, help='The Metamist project name.')
-    parser.add_argument('--output', required=True, help='Path of the primary output file/dir.')
-    parser.add_argument('--type', required=True, help='Analysis type, must be from valid enum.')
-    parser.add_argument('--secondary', nargs='+', help='Optional, list of k=v pairs for secondary analyses.', default=[])
-    parser.add_argument('--meta', nargs='+', help='List of k=v metadata pairs, optional.', default=[])
-    parser.add_argument('--cohorts', nargs='+', help='Metamist cohort ID(s).')
-    parser.add_argument('--sgs', nargs='+', help='Metamist Sequencing Group ID(s).')
-    parser.add_argument('--dry', action='store_true', help='Dry run, print only.')
-    args = parser.parse_args()
-
-    if args.cohorts and args.sgs:
-        raise Exception('Cannot specify both --cohorts and --sgs')
-
-    if not (args.cohorts or args.sgs):
-        raise Exception('Must specify either --cohorts or --sgs')
-
-    query = """
+UPDATE_QUERY = """
     mutation updateAnalysis($project: String!, $analysis:AnalysisInput!) {
         analysis {
             createAnalysis(project:$project, analysis:$analysis) {
@@ -99,46 +67,172 @@ def main():
     }
     """
 
-    outputs = create_output_block(
-        primary=args.output,
-        type=args.type,
-        secondary=args.secondary,
+
+def parse_cli_kv(input_kv: list[str]) -> dict[str, str]:
+    """
+    Takes a list of key-value pairs and parses them into a dictionary.
+    Used in populating both meta and secondary files.
+
+    Args:
+        input_kv: list[str] a list of key-value pairs passed form the CLI
+
+    Returns:
+        a dictionary of key-value pairs built from the original strings
+    """
+    broken_kv = {}
+    for keyvaluepair in input_kv:
+        if '=' not in keyvaluepair:
+            raise ValueError(f'Found key=value entry which lacks a "=": {keyvaluepair}')
+
+        key, value = keyvaluepair.split('=')
+        if key in broken_kv:
+            raise ValueError(f'Duplicate key provided: {key}')
+        broken_kv[key] = value
+    return broken_kv
+
+
+def create_output_block(
+    primary: str,
+    secondary: dict[str, str],
+) -> dict:
+    """Populates the output dict based on the provided arguments."""
+
+    # create the outputs dictionary, with the primary file's full path at the root
+    outputs: dict = {'basename': primary}
+
+    # take the dict of type: file secondary files, and add to the analysis
+    if secondary:
+        outputs['secondary_files'] = {
+            key: {'basename': value} for key, value in secondary.items()
+        }
+
+    return outputs
+
+
+def cli_main():
+    parser = argparse.ArgumentParser(description='Record MultiQC results to Metamist.')
+    parser.add_argument(
+        '--project',
+        required=True,
+        help='The Metamist project name.',
+    )
+    parser.add_argument(
+        '--output',
+        required=True,
+        help='Path of the primary output file/dir.',
+    )
+    parser.add_argument(
+        '--type',
+        required=True,
+        help='Analysis type, must be from valid enum.',
+    )
+    parser.add_argument(
+        '--secondary',
+        nargs='+',
+        help='Optional, list of k=v pairs for secondary analyses.',
+        default=[],
+    )
+    parser.add_argument(
+        '--meta',
+        nargs='+',
+        help='List of k=v metadata pairs, optional.',
+        default=[],
+    )
+    parser.add_argument(
+        '--cohorts',
+        nargs='+',
+        help='Metamist cohort ID(s).',
+        default=[],
+    )
+    parser.add_argument(
+        '--sgs',
+        nargs='+',
+        help='Metamist Sequencing Group ID(s).',
+        default=[],
+    )
+    parser.add_argument(
+        '--dry',
+        action='store_true',
+        help='Dry run, print only.',
+    )
+    args = parser.parse_args()
+
+    # do some transformation of the input meta/secondary file dictionaries
+    meta_dict = parse_cli_kv(args.meta)
+    secondary_dict = parse_cli_kv(args.secondary)
+
+    # call the main analysis generation method
+    create_new(
+        project=args.project,
+        output=args.output,
+        analysis_type=args.type,
+        cohorts=args.cohorts,
+        sgs=args.sgs,
+        dry=args.dry,
+        meta=meta_dict,
+        secondary=secondary_dict,
     )
 
-    variables = {
-        'project': args.project,
+
+def create_new(
+    project: str,
+    output: str,
+    analysis_type: str,
+    meta: dict[str, str] | None = None,
+    secondary: dict[str, str] | None = None,
+    cohorts: list[str] | None = None,
+    sgs: list[str] | None = None,
+    dry: bool = False,
+) -> None:
+    """
+    main method, takes the provided inputs and creates a new analysis entry
+
+    Args:
+        project: str, the name of the project to create the analysis entry in
+        output: str, the primary output path of the analysis entry
+        analysis_type: str, the analysis type (must be from valid enum in metamist)
+        meta: optional dict, if provided this will be added as the analysis.meta dictionary
+        secondary: optional dict, if provided each element of this {type: string} dict will be added as a secondary file
+        cohorts: optional list, COHort IDs to attribute the analysis to. Mutually exclusive with sgs
+        sgs: optional list, Sequencing Group IDs to attribute the analysis to. Mutually exclusive with cohorts
+        dry: bool, if True, payload is printed, but not sent
+    """
+    # fail if the cohorts and sgs are both applied, or if neither is applied
+    if cohorts and sgs:
+        raise Exception('Cannot specify both --cohorts and --sgs')
+
+    if not (cohorts or sgs):
+        raise Exception('Must specify either --cohorts or --sgs')
+
+    outputs = create_output_block(
+        primary=output,
+        secondary=secondary or {},
+    )
+
+    variables: dict = {
+        'project': project,
         'analysis': {
-            'type': args.type,
+            'type': analysis_type,
             'status': 'COMPLETED',
             'outputs': outputs,
-            'meta': {}
-        }
+            'meta': meta,
+            'cohortIds': cohorts or None,
+            'sequencingGroupIds': sgs or None,
+        },
     }
 
-    # allow arbitrary values to be passed into meta. Not sure here how best to tolerate numerical values, if we have 'em
-    if args.meta:
-        meta_kv: dict[str, str] = {}
-        for keyvaluepair in args.meta:
-            key, value = keyvaluepair.split('=')
-            meta_kv[key] = value
-        variables['analysis']['meta'] = meta_kv
-
-    if args.cohorts:
-        variables['analysis']['cohortIds'] = args.cohorts
-    if args.sgs:
-        variables['analysis']['sequencingGroupIds'] = args.sgs
-
-    print(json.dumps(variables, indent=2))
-
-    if args.dry:
+    if dry:
+        print('DRY RUN, would have POSTed the following analysis data:')
+        print(json.dumps(variables, indent=2))
         sys.exit(0)
 
     try:
-        result = graphql.query(query, variables)
+        result = graphql.query(UPDATE_QUERY, variables)
         print(f'Successfully recorded analysis to Metamist: {result}')
-    except Exception as e:
+    except exceptions.ApiException as e:
         print(f'Error executing GraphQL query: {e}', file=sys.stderr)
         sys.exit(1)
 
+
 if __name__ == '__main__':
-    main()
+    cli_main()
